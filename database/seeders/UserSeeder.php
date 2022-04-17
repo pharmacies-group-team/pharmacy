3b146e09254146ad700844185d4be62047129f4f
Ahlam-Mohammed <ahlam.m.alfashq@gmail.com>
2022-04-18 01:16:27 +0300

    Seeder: create users, roles, permissions

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Enum\RoleEnum;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create(
+        [
+            'name' =>"super",
+            'email' => 'super@gmail.com',
+            'email_verified_at' => now(),
+            'password' => bcrypt('123456789')
+
+        ])->assignRole(RoleEnum::SUPER_ADMIN);
+
+        User::create(
+            [
+                'name' =>"pharmacy",
+                'email' => 'pharmacy@gmail.com',
+                'email_verified_at' => now(),
+                'password' => bcrypt('123456789')
+
+            ])->assignRole(RoleEnum::PHARMACY);
+
+        User::create(
+            [
+                'name' =>"client",
+                'email' => 'client@gmail.com',
+                'email_verified_at' => now(),
+                'password' => bcrypt('123456789')
+
+            ])->assignRole(RoleEnum::CLIENT);
+
+
+
+    }
+}
