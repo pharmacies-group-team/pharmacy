ad17d9e9cc811dee44419a1a616f2f2e0c27473c
Naif <nayef7138@gmail.com>
2022-04-19 23:21:27 +0300

    add: Ads model and migration

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -68,5 +69,13 @@ class User extends Authenticatable
     public function payments(): BelongsToMany
     {
         return $this->belongsToMany(Payment::class, 'payment_users');
+    }
+
+    /**
+     * get ads
+     */
+    public function ads(): HasMany
+    {
+        return $this->hasMany(Ad::class);
     }
 }
