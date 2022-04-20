4708325f701f1359af229a66159a38a61664ea82
EnasHameed <alqaadienas@gmail.com>
2022-04-20 11:43:56 +0300

    fix2

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,8 +9,8 @@ use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
-use Spatie\Permission\Traits\HasRoles;
 
+use Spatie\Permission\Traits\HasRoles;
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable, HasRoles;
