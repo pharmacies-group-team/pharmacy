aa28a22ec82bba61c4678a7658280544b5570398
Ahlam-Mohammed <ahlam.m.alfashq@gmail.com>
2022-04-23 23:02:24 +0300

    Frontend: Home Page

---
+++ resources/views/layouts/web/style.blade.php
@@ -0,0 +1,20 @@
+{{-- semantic --}}
+<link rel="stylesheet" type="text/css" href="{{ asset('css/semantic.min.css') }}">
+
+{{-- Bootstrap --}}
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+<!-- BootStrap Icons -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
+
+<!-- Font -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Tajawal:wght@300;400;500;700;800&display=swap" rel="stylesheet">
+
+<!-- owl carousel -->
+<link rel="stylesheet" href="{{ asset('css/owl.carousel.min.css') }}">
+
+<!-- Custom Style -->
+<link rel="stylesheet" href="{{ asset('css/style.css') }}">
+<link rel="stylesheet" href="{{ asset('css/responsive.css') }}">
