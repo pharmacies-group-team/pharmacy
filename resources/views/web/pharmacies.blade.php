94e1ac23554eff6b11a1bf8518b9b026a6d29181
Naif <nayef7138@gmail.com>
2022-04-25 05:40:53 +0300

    fix: pharmacies page

--- resources/views/web/pharmacies.blade.php
+++ resources/views/web/pharmacies.blade.php
@@ -36,41 +36,6 @@
             </div>
           </div>
         </div>
-        <div class="col-12 mt-3">
-          <div class="row">
-            @if (isset($pharmacies))
-              @foreach ($pharmacies as $pharmacy)
-                <div class="col-lg-3">
-                  <section class="card bg-secondary-light rounded-3 card--hover shadow">
-                    <div class="d-flex flex-column justify-content-center align-items-center p-4">
-                      <img src="images/1.png" width="50%" class="rounded-circle img-fluid" alt="pharmacy image">
-
-                      <h3 class="fw-bold text-primary-dark mt-4">{{ $pharmacy->name }}</h3>
-
-                      <p class="text-dark-100">
-                        <span class="text-dark-50">
-                          {{ $pharmacy->neighborhood->directorate->city->name }}
-                        </span>
-                        /
-                        <span>{{ $pharmacy->neighborhood->name }}</span>
-                      </p>
-                    </div>
-
-                    <a class="btn bg-secondary-dark rounded-button-3 fw-bold text-dark-50 p-3"
-                      href="{{ route('pharmacies') }}">
-                      أطلب دوائك
-                    </a>
-                  </section>
-
-                  <section class="card bg-secondary-light rounded-3 card--hover shadow">
-
-
-                  </section>
-                </div>
-              @endforeach
-            @endif
-          </div>
-        </div>
       </div>
     </div>
   </section>
