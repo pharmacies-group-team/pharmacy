997a71366378b177caed8ef011cbe4a243753bfb
Naif <nayef7138@gmail.com>
2022-04-26 06:27:06 +0300

    fix: add new services

--- resources/views/layouts/dashboard/client-dashboard-master.blade.php
+++ resources/views/layouts/dashboard/client-dashboard-master.blade.php
@@ -2,31 +2,29 @@
 <html lang="ar" dir="rtl">
 
 <head>
-    {{-- header --}}
+  @include('layouts/dashboard/header')
 
-    @include('layouts/dashboard/header')
-    <link href="{{ asset('css/dashboard/client.css') }}" rel="stylesheet" />
+  <link href="{{ asset('css/dashboard/client.css') }}" rel="stylesheet" />
 
 </head>
 
 <body data-theme="default" data-layout="fluid" data-sidebar-position="right" data-sidebar-layout="compact">
 
-    <div class="wrapper">
-        {{-- sidebar --}}
-
-        @include('layouts/dashboard/client-sidebar')
-
-
-        {{-- main --}}
-        <div class="main">
-            {{-- navbar --}}
-            @include('layouts/dashboard/navbar')
-            @yield('content')
+  <div class="wrapper">
+    {{-- sidebar --}}
+    {{-- @include('layouts/dashboard/client-sidebar') //TODO --}}
 
 
+    {{-- main --}}
+    <div class="main">
+      {{-- navbar --}}
+      @include('layouts/dashboard/navbar')
+      @yield('content')
 
-        </div>
 
-        {{-- footer --}}
 
-        @include('layouts/dashboard/footer')
+    </div>
+
+    {{-- footer --}}
+
+    @include('layouts/dashboard/footer')
