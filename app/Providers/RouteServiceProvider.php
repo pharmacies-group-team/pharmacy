d63e05ab42beac7b7ecb9d8d3e20c3a27a875266
Ahlam-Mohammed <ahlam.m.alfashq@gmail.com>
2022-04-26 06:45:45 +0300

    Verification

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,8 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/';
+    public const HOME   = '/';
+    public const VERIFY = '/email/verify';
 
     /**
      * Define your route model bindings, pattern filters, etc.
--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -1,7 +1,7 @@
-@extends('layouts.app')
+@extends('layouts.web.master')
 
 @section('content')
-<div class="container">
+<div class="container vh-100">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
