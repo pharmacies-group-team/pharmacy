3afe3aae2b871ceef376a657ad7dd1eaccece3ab
Naif <nayef7138@gmail.com>
2022-05-01 02:36:08 +0300

    update: mix settings

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,10 +11,10 @@ const mix = require("laravel-mix");
  |
  */
 
-mix
-  .js("resources/js/app.js", "public/js")
-  .sass("resources/sass/app.scss", "public/css")
-  .sass("resources/sass/index.scss", "public/css")
-  .sourceMaps();
+// mix
+//   .js("resources/js/app.js", "public/js")
+//   .sass("resources/sass/app.scss", "public/css")
+//   .sass("resources/sass/index.scss", "public/css")
+//   .sourceMaps();
 
 mix.browserSync("127.0.0.1:8000");
