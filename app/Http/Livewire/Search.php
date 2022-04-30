b7d65614d6d8ad06b7add36ceff64a3f4bf8b3a1
Hamzah Al-Hagi <ha.773774142@gmail.com>
2022-04-30 03:19:17 +0300

    the Search

--- app/Http/Livewire/Search.php
+++ app/Http/Livewire/Search.php
@@ -10,7 +10,15 @@ class Search extends Component
   public $pharmacies;
     public function render()
     {
-      $this->pharmacies = Pharmacy::all();
+      // $this->pharmacies = Pharmacy::all();
+      //   return view('livewire.search');
+
+        $searchTerm = '%' .$this->searchTerm. '%';
+        $this->pharmacies = Pharmacy::where('name', 'like',$searchTerm)->get();
         return view('livewire.search');
+          //  $request-query('pharmacy');
+          // $pharmacy = Pharmacywhere('name','LIKE','%'.$name.'%')-get();
+          // return response($pharmacy);
+
     }
 }
