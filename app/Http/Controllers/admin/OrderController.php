89a59e5d6fa935f9458aeebaab94d46093f2d272
Shatha <65645778+shatha-albajly@users.noreply.github.com>
2022-05-02 17:25:14 +0300

    edit admin orders

--- app/Http/Controllers/admin/OrderController.php
+++ app/Http/Controllers/admin/OrderController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers\admin;
+use Illuminate\Support\Facades\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Models\Order;
@@ -10,7 +11,9 @@ class OrderController extends Controller
 {
   public function index()
   {
-    $orders = Order::select()->with('address', 'user', 'pharmacy')->get();
+
+    $orders=Order::select('id','user_id','pharmacy_id','address_id','status','created_at')->with(['user:id,name,avatar','pharmacy:id,name','pharmacy:id,name','address:id,desc'])->get();
+
     // return response($orders);
     return view('admin.orders', compact('orders'));
 
