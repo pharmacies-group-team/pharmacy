780af6943ce45e916bf33934f87b2d3bb62d6ecb
Naif <nayef7138@gmail.com>
2022-05-04 21:20:30 +0300

    add: pharmacy dashboard files

---
+++ resources/views/layouts/pharmacy/master.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="ar" dir="rtl">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>صيدلية اون لاين</title>
+
+  <link href="{{ asset('css/dashboard.css') }}" rel="stylesheet" />
+
+</head>
+
+<body>
+  {{-- load alpinejs before any html element #fix modal light show/hide issues --}}
+  <script src="{{ asset('js/alpine.min.js') }}"></script>
+
+  <div class="dashboard" x-data="{ isSidebarOpen: window.innerWidth >= 786 ? true : false }">
+    {{-- sidebar --}}
+    @include('layouts.dashboard.sidebar')
+
+    <div class="dashboard-content">
+      {{-- navbar --}}
+      @include('layouts.dashboard.navbar')
+
+      @yield('content')
+    </div>
+  </div>
+
+</body>
+
+</html>
--- resources/views/pharmacy/dashboard/index.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.web.user-dashboard.master')
-
-@section('title') Dashboard @stop
-
-@section('content')
-
-@endsection
