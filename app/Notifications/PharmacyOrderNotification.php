0408f2efac309d95fb0525b98e3e90bb97e43e95
Ahlam-Mohammed <ahlam.m.alfashq@gmail.com>
2022-05-05 02:35:54 +0300

    Rename class UserOrderNotification into PharmacyOrderNotification

--- app/Notifications/PharmacyOrderNotification.php
+++ app/Notifications/PharmacyOrderNotification.php
@@ -9,7 +9,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class UserOrderNotification extends Notification implements ShouldQueue
+class PharmacyOrderNotification extends Notification implements ShouldQueue
 {
     use Queueable;
 
