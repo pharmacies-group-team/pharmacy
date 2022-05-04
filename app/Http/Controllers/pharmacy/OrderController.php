986db7dd9f5a0262922002ff72a5f89d4070c74b
Naif <nayef7138@gmail.com>
2022-05-04 23:59:03 +0300

    update: pharmacy/orders add fake data

--- app/Http/Controllers/pharmacy/OrderController.php
+++ app/Http/Controllers/pharmacy/OrderController.php
@@ -5,22 +5,41 @@ namespace App\Http\Controllers\pharmacy;
 use App\Enum\OrderEnum;
 use App\Http\Controllers\Controller;
 use App\Models\Order;
+use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 
 class OrderController extends Controller
 {
     public function getAll()
     {
-        $orders = Auth::user()->pharmacyOrders()->get();
-        return response($orders);
+        // TODO Bug
+        // $orders = Auth::user()->pharmacyOrders()->get();
+        $orders = [
+            (object) [
+                'id' => 1,
+                'status' => 1,
+                'client' => 'ali',
+                'date' => '2002/2/10',
+            ],
+            (object) [
+                'id' => 2,
+                'status' => 1,
+                'client' => 'Ahmed',
+                'date' => '2002/2/10',
+            ]
+        ];
+
+        // echo $orders[0]->id;
+        // exit;
+
+        return view('pharmacy.dashboard.orders', compact('orders'));
     }
 
     public function orderRefusal($id)
     {
         $order = Order::find($id);
 
-        if ($order)
-        {
+        if ($order) {
             $order->update(['status' => OrderEnum::REFUSAL_ORDER]);
             return back()->with('status', 'لقد تم رفض الطلب');
         }
