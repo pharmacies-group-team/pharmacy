135b2c9cab764d7b807ee3439b0a2d702230a20a
Ahlam-Mohammed <ahlam.m.alfashq@gmail.com>
2022-05-06 07:23:01 +0300

    CRUD payment method

--- resources/views/layouts/dashboard/sidebar.blade.php
+++ resources/views/layouts/dashboard/sidebar.blade.php
@@ -102,6 +102,22 @@
       </a>
     </li>
 
+    {{-- Payment Method --}}
+    <li>
+      <a class="list-item-link @if (Route::currentRouteName() === 'admin.payments.index') active @endif" href="{{ route('admin.payments.index') }}">
+        <div class="list-item-icon">
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+               stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+               class="feather feather-bell align-middle">
+            <path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"></path>
+            <path d="M13.73 21a2 2 0 0 1-3.46 0"></path>
+          </svg>
+        </div>
+
+        <span>إدارة طرق الدفع</span>
+      </a>
+    </li>
+
     {{-- account settings --}}
     <li>
       <a class="list-item-link @if (Route::currentRouteName() === 'admin.account-settings') active @endif"
