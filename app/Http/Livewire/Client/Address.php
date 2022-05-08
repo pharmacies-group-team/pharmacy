68bc9f4e598970873d0d9a5562576a1ce30f1531
Ahlam-Mohammed <ahlam.m.alfashq@gmail.com>
2022-05-08 22:17:07 +0300

    Update address

--- app/Http/Livewire/Client/Address.php
+++ app/Http/Livewire/Client/Address.php
@@ -9,7 +9,7 @@ use App\Models\Address as Addresses;
 class Address extends Component
 {
     public $name, $phone, $type_address, $desc;
-    public $addresses;
+    public $addresses, $address_id;
 
     public function render()
     {
@@ -36,6 +36,27 @@ class Address extends Component
 
         $this->resetInputFields();
         session()->flash('message', 'تم إضافة عنوان جديد.');
+    }
+
+    public function edit($id)
+    {
+      $address = Addresses::findOrFail($id);
+
+      $address->update([
+        'name'         => $this->name,
+        'phone'        => $this->phone,
+        'desc'         => $this->desc,
+        'type_address' => $this->type_address,
+      ]);
+
+      $this->resetInputFields();
+      session()->flash('message', 'تم التعديل بنجاح.');
+    }
+
+    public function delete($id)
+    {
+      Addresses::find($id)->delete();
+      session()->flash('message', 'تم الحذف بنجاح.');
     }
 
     public function resetInputFields()
