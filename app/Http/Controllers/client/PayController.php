92978a998f0b3620c14c1592e89d2abe99d79ee9
Ahlam-Mohammed <ahlam.m.alfashq@gmail.com>
2022-05-09 00:49:04 +0300

    Payment (TODO 😣)

--- app/Http/Controllers/client/PayController.php
+++ app/Http/Controllers/client/PayController.php
@@ -11,7 +11,7 @@ use Illuminate\Support\Facades\Http;
 
 class PayController extends Controller
 {
-    public function paymentt(Request $request)
+    public function payment(Request $request)
     {
       $products = QuotationDetails::where('quotation_id', $request->quotation_id)
         ->select('id', 'product_name As name', 'quantity', 'price As unit_amount')->get();
@@ -34,7 +34,7 @@ class PayController extends Controller
           'metadata'        => $quotation->order->user
         ];
 
-//        return response($paymentData);
+        return response($paymentData);
 
         $secretKey      = 'rRQ26GcsZzoEhbrP2HZvLYDbn9C9et';
         $publishableKey = 'HGvTMLDssJghr9tlN9gr4DVYt0qyBy';
@@ -49,11 +49,9 @@ class PayController extends Controller
             CURLOPT_TIMEOUT => 30000,
             CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
             CURLOPT_CUSTOMREQUEST => "POST",
-            CURLOPT_POSTFIELDS => json_encode($paymentData),
+            CURLOPT_POSTFIELDS => $paymentData,
             CURLOPT_HTTPHEADER => array(
-              "accept: */*",
-              "accept-language: en-US,en;q=0.8",
-              'content-Type: application/json',
+              'content-Type: application/x-www-form-urlencoded',
               'private_key:' . $secretKey,
               'public_key:' . $publishableKey,
           ),
@@ -63,11 +61,11 @@ class PayController extends Controller
         $err = curl_error($curl);
         curl_close($curl);
 
-        if ($err)
-        {
-          return ['result' => 'error', 'message' => $err];
+        if ($err) {return ['result' => 'error', 'message' => $err];}
+        else {
+          $response = json_decode($result, true);
+          return $response;
         }
-        else return $result;
       }
 
 
@@ -75,37 +73,37 @@ class PayController extends Controller
     }
 
 
-    public function payment(Request $request)
-    {
-      $products = QuotationDetails::where('quotation_id', $request->quotation_id)
-        ->select('id', 'product_name As name', 'quantity', 'price As unit_amount')->get();
-
-      $quotation = Quotation::find($request->quotation_id)->first();
-
-      $paymentData =
-        [
-          'order_reference' => (string) $quotation->order->id,
-          'products'        => $products,
-          'currency'        => 'YER',
-          'total_amount'    => (string) $quotation->total,
-          'success_url'     => 'http://127.0.0.1:8000/client/success',
-          'cancel_url'      => 'http://127.0.0.1:8000/client/cancel',
-          'metadata'        => $quotation->order->user
-        ];
-
-      $secretKey      = 'rRQ26GcsZzoEhbrP2HZvLYDbn9C9et';
-      $publishableKey = 'HGvTMLDssJghr9tlN9gr4DVYt0qyBy';
-
-      $waselAPI = 'https://waslpayment.com/api/test/merchant/payment_order';
-
-      $response = Http::withHeaders([
-        'content-Type' => 'application/json',
-        'private_key' => $secretKey,
-        'public_key' => $publishableKey,
-      ])->post($waselAPI, $paymentData);
-
-      dd($response->status());
-    }
+//    public function paymesnt(Request $request)
+//    {
+//      $products = QuotationDetails::where('quotation_id', $request->quotation_id)
+//        ->select('id', 'product_name As name', 'quantity', 'price As unit_amount')->get();
+//
+//      $quotation = Quotation::find($request->quotation_id)->first();
+//
+//      $paymentData =
+//        [
+//          'order_reference' => (string) $quotation->order->id,
+//          'products'        => $products,
+//          'currency'        => 'YER',
+//          'total_amount'    => (string) $quotation->total,
+//          'success_url'     => 'http://127.0.0.1:8000/client/success',
+//          'cancel_url'      => 'http://127.0.0.1:8000/client/cancel',
+//          'metadata'        => $quotation->order->user
+//        ];
+//
+//      $secretKey      = 'rRQ26GcsZzoEhbrP2HZvLYDbn9C9et';
+//      $publishableKey = 'HGvTMLDssJghr9tlN9gr4DVYt0qyBy';
+//
+//      $waselAPI = 'https://waslpayment.com/api/test/merchant/payment_order';
+//
+//     return Http::withHeaders([
+//        'content-Type' => 'application/x-www-form-urlencoded',
+//        'private_key' => $secretKey,
+//        'public_key' => $publishableKey,
+//      ])->post($waselAPI, $paymentData);
+//
+////      dd($response->status());
+//    }
 
     public function cancel()
     {
