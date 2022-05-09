68e3b4ec54eac31c122e69c986efcc1ed2c297ec
Hamzah Al-Hagi <ha.773774142@gmail.com>
2022-05-09 14:46:26 +0300

    pragraph

--- resources/views/web/about-us.blade.php
+++ resources/views/web/about-us.blade.php
@@ -4,7 +4,12 @@
 
 @section('content')
 <div class="container my-5">
-  <p dir="RTL" class="text-primary-darker fw-bold fs-3" >عن منصة صيدليه اون لين</p>
+  <h1 class="text-primary-darker fw-bold fs-3" >عن منصة صيدليه اون لين</h1>
+  <p class="text-primary-dark mt-4">
+      هدفنا تسهيل عملية شراء الدواء كما نسعى الى توفير الدواء الندره لطالبيها
+      و تعريف الصيدلية با الادوية التي يجب توفيرها
+      وتوفير ادويه دوريه لاصحاب الامرض  الامزمنه
+  </p>
 </div>
 
 
