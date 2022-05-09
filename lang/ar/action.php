ccd17898e30d5ebe6b9e6cecd11c17e111b6b0f9
naif <nayef7138@gmail.com>
2022-05-09 15:35:46 +0300

    update: pharmacy order quotation

--- lang/ar/action.php
+++ lang/ar/action.php
@@ -2,9 +2,11 @@
 
 return [
   "show" => "عرض",
+  "send" => "إرسال",
   "not-found" => "غير متواجد",
   "send-message" => "إرسال رسالة",
   "create-quote" => "إنشاء عرض سعر",
-  "order-details" => "تفاصيل الطلب"
+  "order-details" => "تفاصيل الطلب",
+  "add" => "إضافة"
   // "" => ""
 ];
