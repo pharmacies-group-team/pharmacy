bdc80d6727668893ff9b1ab207d165c0b40ae02d
Hamzah Al-Hagi <ha.773774142@gmail.com>
2022-05-09 13:49:05 +0300

    title

--- resources/views/web/about-us.blade.php
+++ resources/views/web/about-us.blade.php
@@ -3,7 +3,9 @@
 @section('title') About US @stop
 
 @section('content')
-
+<div class="normal-content">
+  <p dir="RTL" style="text-align: right;"><strong><font face="DINNextLTW23-Regular" color="#12B3AB">عن منصة صيدليه اون لين</font></strong></p>
+</div>
 
 
 
