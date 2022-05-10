ce7de42ace3365c5f65f9911e583b1173f1ef3b2
naif <nayef7138@gmail.com>
2022-05-10 22:15:39 +0300

    Update lang/heading.php

--- lang/ar/heading.php
+++ lang/ar/heading.php
@@ -4,5 +4,26 @@
 return [
   "profile" => "بروفايل",
   "protection-and-privacy" => "الحماية والخصوصية",
-  "order-details" => "تفاصيل الطلب"
+  "order-details" => "تفاصيل الطلب",
+  "invoice" => "فاتورة",
+  "invoice_id" => "رقم فاتورة",
+  "invoice-from" => "invoice from",
+  "invoice-to" => "invoice to",
+  "taxes" => "الضرائب",
+  "subtotal" => "المجموع الفرعي",
+  "total" => "الاجمالي",
+  "date" => "تاريخ",
+
+  "product-name" => "name",
+  "product-description" => "description",
+  "product-cost" => "cost",
+  "product-amount" => "amount",
+  "product-price" => "price",
+
+
+  // "" => "",
+  // "" => "",
+  // "" => "",
+  // "" => "",
+
 ];
