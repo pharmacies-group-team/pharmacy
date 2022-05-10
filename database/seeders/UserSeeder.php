2adfe99990b8385839dd360b52f1037fa3faae8a
naif <nayef7138@gmail.com>
2022-05-10 20:48:04 +0300

    Update UserSeeder.php

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Enum\RoleEnum;
+use App\Models\Address;
 use App\Models\Pharmacy;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
@@ -18,40 +19,66 @@ class UserSeeder extends Seeder
     public function run()
     {
         User::create(
-        [
-            'name' =>"super",
-            'email' => 'super@gmail.com',
-            'email_verified_at' => now(),
-            'password' => bcrypt('123456789')
+            [
+                'name' => "super",
+                'email' => 'super@gmail.com',
+                'email_verified_at' => now(),
+                'password' => bcrypt('123456789')
 
-        ])->assignRole(RoleEnum::SUPER_ADMIN);
+            ]
+        )->assignRole(RoleEnum::SUPER_ADMIN);
 
         $pharmacy = User::create(
-          [
-              'name' =>"pharmacy",
-              'email' => 'pharmacy@gmail.com',
-              'email_verified_at' => now(),
-              'password' => bcrypt('123456789')
+            [
+                'name' => "pharmacy",
+                'email' => 'pharmacy@gmail.com',
+                'email_verified_at' => now(),
+                'password' => bcrypt('123456789')
 
-          ])->assignRole(RoleEnum::PHARMACY);
+            ]
+        )->assignRole(RoleEnum::PHARMACY);
 
         Pharmacy::create(
-        [
-          'name'            => 'Dreams',
-          'user_id'         => $pharmacy->id,
-        ]);
+            [
+                'name'            => 'Dreams',
+                'user_id'         => $pharmacy->id,
+            ]
+        );
 
         User::create(
             [
-                'name' =>"client",
+                'name' => "client",
                 'email' => 'client@gmail.com',
                 'email_verified_at' => now(),
                 'password' => bcrypt('123456789')
 
-            ])->assignRole(RoleEnum::CLIENT);
+            ]
+        )->assignRole(RoleEnum::CLIENT);
 
         User::factory()->count(50)->create();
 
+        // add user address
+        Address::insert(
+            [
+                [
+                    "name"          => "Naif",
+                    "phone"         => 777888999,
+                    "type_address"  => "home",
+                    "desc"          => "Home is home for us",
+                    "user_id"       => 3
+                ],
+                [
+                    "name"          => "Naif",
+                    "phone"         => 777888999,
+                    "type_address"  => "home",
+                    "desc"          => "Home is home for us",
+                    "user_id"       => 3
+                ]
+            ]
+        );
+
+
+        // user_id
 
     }
 }
