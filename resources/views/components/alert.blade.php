d869d5930d2091a5738ab9cbac9744467a0d282e
naif <nayef7138@gmail.com>
2022-05-12 11:45:20 +0300

    add animation for alerts

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -1,31 +1,34 @@
 @if (isset($type))
-  {{-- alert message --}}
-  @if ($type === 'message' && session()->has('message'))
-    <div class="alert-box" x-init="setTimeout(() => $refs.alertErrorMessage.parentNode.removeChild($refs.alertErrorMessage), 3000)" x-ref="alertErrorMessage">
-      {{ session('message') }}
-    </div>
+  <div
+    x-data='{animationClasses: `animate__bounce animate__animated animate__repeat-3`, hidden:() => setTimeout(() => $refs.alertErrorMessage.parentNode.removeChild($refs.alertErrorMessage), 5000)}'>
+    {{-- alert message --}}
+    @if ($type === 'message' && session()->has('message'))
+      <div class="alert-box" :class="animationClasses" x-init=" hidden()" x-ref="alertErrorMessage">
+        {{ session('message') }}
+      </div>
 
 
-    {{-- status alert --}}
-  @elseif ($type === 'status' && session()->has('status'))
-    <div class="alert-box" x-init="setTimeout(() => $refs.alertErrorMessage.parentNode.removeChild($refs.alertErrorMessage), 3000)" x-ref="alertErrorMessage">
-      {{ session('status') }}
-    </div>
+      {{-- status alert --}}
+    @elseif ($type === 'status' && session()->has('status'))
+      <div class="alert-box" :class="animationClasses" x-init=" hidden()" x-ref="alertErrorMessage">
+        {{ session('status') }}
+      </div>
 
 
-    {{-- success alert --}}
-  @elseif ($type === 'success' && session()->has('success'))
-    <div class="alert-box" x-init="setTimeout(() => $refs.alertErrorMessage.parentNode.removeChild($refs.alertErrorMessage), 3000)" x-ref="alertErrorMessage">
-      {{ session('success') }}
-    </div>
+      {{-- success alert --}}
+    @elseif ($type === 'success' && session()->has('success'))
+      <div class="alert-box" :class="animationClasses" x-init=" hidden()" x-ref="alertErrorMessage">
+        {{ session('success') }}
+      </div>
 
 
-    {{-- any errors --}}
-     @elseif($type === 'any' && $errors->any())
-    <div class="alert-box" x-init="setTimeout(() => $refs.alertErrorMessage.parentNode.removeChild($refs.alertErrorMessage), 3000)" x-ref="alertErrorMessage">
-      يُرجى التأكد من إدخال البيانات الصحيحة
-    </div>
-  @endif
+      {{-- any errors --}}
+    @elseif($type === 'any' && $errors->any())
+      <div class="alert-box" :class="animationClasses" x-init="hidden()" x-ref="alertErrorMessage">
+        يُرجى التأكد من إدخال البيانات الصحيحة
+      </div>
+    @endif
+  </div>
 @else
   {{-- this only for dev team 🤣 --}}
 
