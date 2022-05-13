cfe02415fa6f1d50a4fc598f3741cc243b2dc049
Ahlam-Mohammed <ahlam.m.alfashq@gmail.com>
2022-05-14 01:35:38 +0300

    Update Order factory

--- database/factories/OrderFactory.php
+++ database/factories/OrderFactory.php
@@ -3,6 +3,10 @@
 namespace Database\Factories;
 
 use App\Enum\OrderEnum;
+use App\Enum\QuotationEnum;
+use App\Models\Order;
+use App\Models\Quotation;
+use App\Models\QuotationDetails;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -18,11 +22,70 @@ class OrderFactory extends Factory
     public function definition()
     {
       return [
-          'order'           => $this->faker->text,
-          'image'           => 'default.jpg',
-          'status'          => OrderEnum::NEW_ORDER,
-          'user_id'         => 3,
-          'pharmacy_id'     => 2
+        'order'           => $this->faker->text,
+        'image'           => 'default.jpg',
+        'status'          => OrderEnum::UNPAID_ORDER,
+        'user_id'         => 3,
+        'pharmacy_id'     => 2
       ];
+    }
+
+    public function configure()
+    {
+      return $this->afterCreating(function (Order $order) {
+
+        $quotation = Quotation::create(
+        [
+          'total'       => 25000,
+          'order_id'    => $order->id,
+          'address_id'  => 1
+        ] );
+
+        QuotationDetails::create(
+          [
+            'product_name' => $this->faker->name,
+            'product_unit' => QuotationEnum::TYPE_CARTONS,
+            'quantity'     => 2,
+            'price'        => 3000,
+            'total'        => 6000,
+            'quotation_id' => $quotation->id
+          ]);
+        QuotationDetails::create(
+          [
+            'product_name' => $this->faker->name,
+            'product_unit' => QuotationEnum::TYPE_CARTONS,
+            'quantity'     => 2,
+            'price'        => 2000,
+            'total'        => 4000,
+            'quotation_id' => $quotation->id
+          ]);
+        QuotationDetails::create(
+          [
+            'product_name' => $this->faker->name,
+            'product_unit' => QuotationEnum::TYPE_CARTONS,
+            'quantity'     => 1,
+            'price'        => 1000,
+            'total'        => 1000,
+            'quotation_id' => $quotation->id
+          ]);
+        QuotationDetails::create(
+          [
+            'product_name' => $this->faker->name,
+            'product_unit' => QuotationEnum::TYPE_CARTONS,
+            'quantity'     => 3,
+            'price'        => 3000,
+            'total'        => 9000,
+            'quotation_id' => $quotation->id
+          ]);
+        QuotationDetails::create(
+          [
+            'product_name' => $this->faker->name,
+            'product_unit' => QuotationEnum::TYPE_CARTONS,
+            'quantity'     => 2,
+            'price'        => 4000,
+            'total'        => 8000,
+            'quotation_id' => $quotation->id
+          ]);
+      });
     }
 }
