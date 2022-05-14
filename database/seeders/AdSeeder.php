c7dbf9bd50c5d76aeb73d4f6dcef3bb384582a5f
naif <nayef7138@gmail.com>
2022-05-14 22:14:54 +0300

    update: seeder files

--- database/seeders/AdSeeder.php
+++ database/seeders/AdSeeder.php
@@ -17,24 +17,34 @@ class AdSeeder extends Seeder
      */
     public function run()
     {
-        Ad::create([
-            'title' => 'one two',
-            'image' => 'default_ad.png',
-            'link' => 'https://google.com',
-            'ad_position' => 'home',
-            'start_at' => '2022-05-01',
-            'end_at' => '2022-05-18',
-            'user_id' => 1
-        ]);
-
-        Ad::create([
-            'title' => 'two three',
-            'image' => 'default_ad.png',
-            'link' => 'https://twitter.com',
-            'ad_position' => 'home',
-            'start_at' => '2022-05-01',
-            'end_at' => '2022-05-18',
-            'user_id' => 1
-        ]);
+        Ad::create(
+            [
+                'title' => 'one',
+                'image' => 'default_ad.png',
+                'link' => 'https://google.com',
+                'ad_position' => 'home',
+                'start_at' => '2022-05-01',
+                'end_at' => '2022-05-18',
+                'user_id' => 1
+            ],
+            [
+                'title' => 'two',
+                'image' => 'default_ad.png',
+                'link' => 'https://twitter.com',
+                'ad_position' => 'home',
+                'start_at' => '2022-05-01',
+                'end_at' => '2022-05-18',
+                'user_id' => 1
+            ],
+            [
+                'title' => 'three',
+                'image' => 'default_ad.png',
+                'link' => 'https://twitter.com',
+                'ad_position' => 'home',
+                'start_at' => '2022-05-01',
+                'end_at' => '2022-05-18',
+                'user_id' => 1
+            ]
+        );
     }
 }
