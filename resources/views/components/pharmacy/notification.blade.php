2fa936de86dfb692b477333d0255a2a7d3af13e5
naif <nayef7138@gmail.com>
2022-05-15 00:19:27 +0300

    move: notification file to component folder

--- resources/views/components/pharmacy/notification.blade.php
+++ resources/views/components/pharmacy/notification.blade.php
@@ -5,8 +5,10 @@
     <header class="t-header">
       <img src="{{ asset(UserEnum::USER_AVATAR_PATH . $notification->data['sender']['avatar']) }}" alt="user avatar"
         class="t-avatar" width="40">
+
       <h4 class="t-name">{{ $notification->data['sender']['name'] }}</h4>
     </header>
+
     <p class="t-desc" style="font-size: 14px; color: #588FF4">{{ $notification->data['message'] }}</p>
   </a>
 </li>
