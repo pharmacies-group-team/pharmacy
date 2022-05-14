32d9fc324a75d46254ac9b48fc007d1b32676aff
Ahlam-Mohammed <ahlam.m.alfashq@gmail.com>
2022-05-14 12:14:06 +0300

    costume styling pagination

--- resources/views/livewire/livewire-pagination.blade.php
+++ resources/views/livewire/livewire-pagination.blade.php
@@ -1,14 +1,14 @@
 @if ($paginator->hasPages())
   <nav>
-    <ul class="pagination">
+    <ul class="pagination pagination-c">
       {{-- Previous Page Link --}}
       @if ($paginator->onFirstPage())
         <li class="page-item disabled" aria-disabled="true" aria-label="@lang('pagination.previous')">
-          <span class="page-link" aria-hidden="true">&lsaquo;</span>
+          <span class="page-link page-link-c radius-right" aria-hidden="true">&lsaquo;</span>
         </li>
       @else
         <li class="page-item">
-          <a class="page-link" wire:click="previousPage" wire:loading.attr="disabled" style="cursor: pointer ;color: #4E7DCB !important;" rel="prev" aria-label="@lang('pagination.previous')">&lsaquo;</a>
+          <a class="page-link page-link-c radius-right" wire:click="previousPage" wire:loading.attr="disabled" style="cursor: pointer ;" rel="prev" aria-label="@lang('pagination.previous')">&lsaquo;</a>
         </li>
       @endif
 
@@ -16,16 +16,16 @@
       @foreach ($elements as $element)
         {{-- "Three Dots" Separator--}}
         @if (is_string($element))
-          <li class="page-item disabled" aria-disabled="true"><span class="page-link">{{ $element }}</span></li>
+          <li class="page-item page-link-c disabled" aria-disabled="true"><span class="page-link">{{ $element }}</span></li>
         @endif
 
         {{-- Array Of Links--}}
         @if (is_array($element))
           @foreach ($element as $page => $url)
             @if ($page == $paginator->currentPage())
-              <li class="page-item active" aria-current="page"><span class="page-link" style="background-color : #4E7DCB !important;">{{ $page }}</span></li>
+              <li class="page-item page-link-c active" aria-current="page"><span class="page-link" style="background-color : #4E7DCB !important;">{{ $page }}</span></li>
             @else
-              <li class="page-item"><a class="page-link" wire:click="gotoPage({{ $url }})" style="color: #4E7DCB !important;">{{ $page }}</a></li>
+              <li class="page-item"><a class="page-link page-link-c" wire:click="gotoPage({{ $url }})" style="color: #4E7DCB !important;">{{ $page }}</a></li>
             @endif
           @endforeach
         @endif
@@ -34,11 +34,11 @@
       {{-- Next Page Link --}}
       @if ($paginator->hasMorePages())
         <li class="page-item">
-          <a class="page-link" wire:click="nextPage" wire:loading.attr="disabled" style="cursor: pointer ;color: #4E7DCB !important;" rel="next" aria-label="@lang('pagination.next')">&rsaquo;</a>
+          <a class="page-link page-link-c radius-left" wire:click="nextPage" wire:loading.attr="disabled" style="cursor: pointer ;" rel="next" aria-label="@lang('pagination.next')">&rsaquo;</a>
         </li>
       @else
         <li class="page-item disabled" aria-disabled="true" aria-label="@lang('pagination.next')">
-          <span class="page-link" aria-hidden="true">&rsaquo;</span>
+          <span class="page-link page-link-c radius-left" aria-hidden="true">&rsaquo;</span>
         </li>
       @endif
     </ul>
