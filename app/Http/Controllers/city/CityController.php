67ce395a4b6d9866cffb8e593a7692437832467b
Hamzah Al-Hagi <ha.773774142@gmail.com>
2022-05-15 15:50:35 +0300

    feat and test: city store function using postman API Teaster

--- app/Http/Controllers/city/CityController.php
+++ app/Http/Controllers/city/CityController.php
@@ -19,7 +19,15 @@ class CityController extends Controller
 
     public function store(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(), [
+          // min:2 becouse some cities in Yemen can be two litter like Ab
+          'name' => 'required|min:2|max:30|string',
+        ]);
+        City::create([
+          'name' => $request->input('name'),
+        ]);
+        return response(['added successfully', $validator->errors()]);
+      //  return redirect()->back()->with('status', 'added successfully');
     }
 
     public function show($id)
