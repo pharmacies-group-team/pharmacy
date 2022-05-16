c117a7d294a5c3afbb6c408cca02ab1bf1d172e5
Ahlam-Mohammed <ahlam.m.alfashq@gmail.com>
2022-05-16 12:13:58 +0300

    financial operations in pharmacy dashboard

--- app/Http/Controllers/pharmacy/DashboardController.php
+++ app/Http/Controllers/pharmacy/DashboardController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\pharmacy;
 
 use App\Http\Controllers\Controller;
 use App\Models\Pharmacy;
+use Bavix\Wallet\Models\Transaction;
 use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
@@ -32,8 +33,15 @@ class DashboardController extends Controller
     }
 
     public function getInvoiceProfile()
-    {
-        $user = Auth::user();
-        return view('pharmacy.invoice-profile', compact('user'));
-    }
+      {
+          $user                  = Auth::user();
+          $transactions          = Transaction::where('payable_id', $user->id)->get();
+          $amount_confirmed      = Transaction::where('payable_id', $user->id)->where('confirmed', 1)->sum('amount');
+          $amount_not_confirmed  = Transaction::where('payable_id', $user->id)->where('confirmed', 0)->sum('amount');
+          $invoice_confirmed     = Transaction::where('payable_id', $user->id)->where('confirmed', 1)->count('amount');
+          $invoice_not_confirmed = Transaction::where('payable_id', $user->id)->where('confirmed', 0)->count('amount');
+
+          return view('pharmacy.invoice-profile',
+            compact('user', 'transactions', 'amount_confirmed','amount_not_confirmed', 'invoice_confirmed', 'invoice_not_confirmed'));
+      }
 }
