8986f93bcb5e9829fe315ba4ea8746b7c17a9502
naif <nayef7138@gmail.com>
2022-05-17 20:12:27 +0300

    update: layouts master

--- resources/views/layouts/pharmacy/master.blade.php
+++ resources/views/layouts/pharmacy/master.blade.php
@@ -4,6 +4,8 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- CSRF Token -->
+  <meta name="csrf-token" content="{{ csrf_token() }}">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>صيدلية اون لاين</title>
 
@@ -27,8 +29,9 @@
   </div>
 
   @livewireScripts()
-
   @include('layouts.shared.scripts')
+
+  @yield('script')
 </body>
 
 </html>
