060e14373dc0f05bc73dac8aafd847ccbf23fbf9
naif <nayef7138@gmail.com>
2022-05-18 15:28:03 +0300

    add: chat relationship functions

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -7,8 +7,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Message extends Model
 {
-    use HasFactory;
-    protected $fillable = [
-      'from', 'to', 'message', 'is_read'
-    ];
+  use HasFactory;
+  protected $fillable = [
+    'from', 'to', 'message', 'is_read'
+  ];
+
+
+  public function toUser()
+  {
+    return $this->belongsTo(User::class, 'to');
+  }
+
+  public function fromUser()
+  {
+    return $this->belongsTo(User::class, 'from');
+  }
 }
