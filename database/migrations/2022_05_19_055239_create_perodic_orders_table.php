33b57999b1576775e469d22831e3e8372e46284d
Shatha <65645778+shatha-albajly@users.noreply.github.com>
2022-05-19 09:40:55 +0300

    update perodic order seeders

--- database/migrations/2022_05_19_055239_create_perodic_orders_table.php
+++ database/migrations/2022_05_19_055239_create_perodic_orders_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use phpDocumentor\Reflection\Types\Nullable;
 
 return new class extends Migration
 {
@@ -17,7 +18,7 @@ return new class extends Migration
       $table->id();
       $table->foreignId('order_id');
       $table->foreign('order_id')->references('id')->on('orders')->onDelete('cascade');
-      $table->date('next_order_date');
+      $table->date('next_order_date')->nullable();
       $table->date('start_date');
       $table->string('perodic_date_type');
       $table->boolean('is_active');
--- database/seeders/PerodicOrderSeeder.php
+++ database/seeders/PerodicOrderSeeder.php
@@ -18,10 +18,10 @@ class PerodicOrderSeeder extends Seeder
   public function run()
   {
     //
-    PerodicOrder::create(
+    PerodicOrder::insert([
       ['order_id' => 1, 'is_active' => 1, 'start_date' => Carbon::create('2022', '05', '21'), 'perodic_date_type' => 'weekly'],
       ['order_id' => 2, 'is_active' => 1, 'start_date' => Carbon::create('2022', '05', '22'), 'perodic_date_type' => 'monthly'],
       ['order_id' => 3, 'is_active' => 0, 'start_date' => Carbon::create('2022', '06', '21'), 'perodic_date_type' => 'weekly']
-    );
+    ]);
   }
 }
