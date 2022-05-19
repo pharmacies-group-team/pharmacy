f1fc59f98d32f4e0aa145681a6c2e83b40f4ee29
naif <nayef7138@gmail.com>
2022-05-19 16:27:55 +0300

    fix: table-dropdown modal fixed issues

--- resources/views/components/table-dropdown.blade.php
+++ resources/views/components/table-dropdown.blade.php
@@ -1,7 +1,9 @@
-<div class="t-table-dropdown" x-data="{ open: false }" @click.away="open = false">
+<div class="t-table-dropdown" :class="open ? 'is-active' : ''" x-data="{ open: false }" @click.away="open = false">
 
-  <button @click="open = true">
-    open
+  <button @click="open = true" class="t-table-dropdown-menu-btn">
+    <div class='icon'>
+      <ion-icon src='{{ asset('images/icons/menu.svg') }}'></ion-icon>
+    </div>
   </button>
 
   <div class="t-table-dropdown-content" x-show="open">
