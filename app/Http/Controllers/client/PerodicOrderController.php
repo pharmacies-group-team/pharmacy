65ec3fb904a2bdae08b24bd82f25722d36c88142
Shatha <65645778+shatha-albajly@users.noreply.github.com>
2022-05-20 22:09:36 +0300

    Update PerodicOrderController.php

--- app/Http/Controllers/client/PerodicOrderController.php
+++ app/Http/Controllers/client/PerodicOrderController.php
@@ -2,17 +2,23 @@
 
 namespace App\Http\Controllers\client;
 
+use App\Http\Controllers\Controller\OrderController;
+
 use App\Http\Controllers\Controller;
+use App\Services\NotificationService;
+
 use App\Models\PerodicOrder;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Carbon\Carbon;
+use App\Models\Order;
+
 use App\Enum\PerodicOrderEnum;
 
 
 class PerodicOrderController extends Controller
 {
-  //
+  // show all perodic task
   public function showTasks()
   {
 
@@ -20,33 +26,30 @@ class PerodicOrderController extends Controller
     return view('client.perodic-orders', compact('perodic_orders'));
   }
 
+
+  // add new perodic task
   public function addTask(Request $request)
   {
     $data = $request->all();
     $perodic_order = new PerodicOrder();
     $perodic_order->user_id = Auth::id();
-
     $perodic_order->order_id = $data['order_id'];
     $perodic_order->start_date = $data['start_date'];
     $perodic_order->perodic_date_type = $data['perodic_date_type'];
-    if ($data['perodic_date_type'] === PerodicOrderEnum::WEEKLY) {
-      $nextDate = Carbon::parse($data['start_date'])->copy()->addWeek();
-    } else if ($data['perodic_date_type'] === PerodicOrderEnum::MONTHLY) {
-      $nextDate = Carbon::parse($data['start_date'])->copy()->addMonth();
-    }
-    $perodic_order->next_order_date = $nextDate;
+    // if ($data['perodic_date_type'] === PerodicOrderEnum::WEEKLY) {
+    //   $nextDate = Carbon::parse($data['start_date'])->copy()->addWeek();
+    // } else if ($data['perodic_date_type'] === PerodicOrderEnum::MONTHLY) {
+    //   $nextDate = Carbon::parse($data['start_date'])->copy()->addMonth();
+    // }
+    $perodic_order->next_order_date = $data['start_date'];
     $perodic_order->is_active = 1;
     $perodic_order->save();
     return back()->with('status', 'تم اضافه الطلب الدوري بنجاح');
   }
 
-  public function setCronJob()
-  {
-    $perodic_orders = Auth::user()->userPerodicOrders()->get();
-    return view('0-testing.perodic-order', compact('perodic_orders'));
-  }
 
 
+  // activate and deactivate perodic orders
   public function togglePerodicOrder(Request $request, $id)
   {
     $perodic_order = PerodicOrder::where('id', $id)->update(['is_active' => !PerodicOrder::find($id)->is_active]);
