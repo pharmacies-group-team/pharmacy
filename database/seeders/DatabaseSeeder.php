f0629f2f1d54bd774ffbb7b37d745b65e5697a11
naif <nayef7138@gmail.com>
2022-05-20 21:55:50 +0300

    fix: seeder file

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -7,22 +7,22 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        // \App\Models\User::factory(10)->create();
-        $this->call([
-            PermissionSeeder::class,
-            CitySeeder::class,
-            UserSeeder::class,
-            WebInfoSeeder::class,
-            PharmacySeeder::class,
-            AdSeeder::class,
-            OrderSeeder::class
-        ]);
-    }
+  /**
+   * Seed the application's database.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    // \App\Models\User::factory(10)->create();
+    $this->call([
+      PermissionSeeder::class,
+      UserSeeder::class,
+      CitySeeder::class,
+      WebInfoSeeder::class,
+      PharmacySeeder::class,
+      AdSeeder::class,
+      OrderSeeder::class
+    ]);
+  }
 }
