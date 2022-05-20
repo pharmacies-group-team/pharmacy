da8c5ad9fe064d475807eee612366ea781ddcb49
Shatha <65645778+shatha-albajly@users.noreply.github.com>
2022-05-20 19:10:33 +0300

    update perodic orders migration and seeder

--- database/migrations/2022_05_19_055239_create_perodic_orders_table.php
+++ database/migrations/2022_05_19_055239_create_perodic_orders_table.php
@@ -16,13 +16,19 @@ return new class extends Migration
   {
     Schema::create('perodic_orders', function (Blueprint $table) {
       $table->id();
+
       $table->foreignId('order_id');
       $table->foreign('order_id')->references('id')->on('orders')->onDelete('cascade');
+
+      $table->foreignId('user_id');
+      $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+
       $table->date('next_order_date')->nullable();
       $table->date('start_date');
       $table->string('perodic_date_type');
       $table->boolean('is_active');
       $table->timestamps();
+      $table->softDeletes();
     });
   }
 
