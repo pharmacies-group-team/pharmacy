e26a4d868bf81264ef532b008f75d411122d0a21
naif <nayef7138@gmail.com>
2022-05-21 00:49:20 +0300

    add: chart.js UI for admin dashboard

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,73 +1,104 @@
 @extends('layouts.admin.master')
 @section('content')
+  <main class="page-admin-report">
+    <div class="t-chart">
 
-  <div class="container mt-4">
-    <div class="row">
-      <div class="col-sm-6">
-        <div class="card widget-flat">
-          <div class="card-body">
-            <div class="float-end">
-              <i class="mdi mdi-account-multiple widget-icon"></i>
-            </div>
-            <h5 class="text-muted fw-normal mt-0" title="Number of Customers">Customers</h5>
-            <h3 class="mt-3 mb-3">36,254</h3>
-            <p class="text-muted mb-0">
-              <span class="text-success me-2"><i class="mdi mdi-arrow-up-bold"></i> 5.27%</span>
-              <span class="text-nowrap">Since last month</span>
-            </p>
-          </div> <!-- end card-body-->
-        </div> <!-- end card-->
-      </div> <!-- end col-->
+      <div class="t-chart-item">
+        <h3 class="t-chart-title">@lang('heading.orders')</h3>
+        <canvas id="chart-orders" width="300" height="300"></canvas>
+      </div>
 
-      <div class="col-sm-6">
-        <div class="card widget-flat">
-          <div class="card-body">
-            <div class="float-end">
-              <i class="mdi mdi-cart-plus widget-icon"></i>
-            </div>
-            <h5 class="text-muted fw-normal mt-0" title="Number of Orders">Orders</h5>
-            <h3 class="mt-3 mb-3">5,543</h3>
-            <p class="text-muted mb-0">
-              <span class="text-danger me-2"><i class="mdi mdi-arrow-down-bold"></i> 1.08%</span>
-              <span class="text-nowrap">Since last month</span>
-            </p>
-          </div> <!-- end card-body-->
-        </div> <!-- end card-->
-      </div> <!-- end col-->
-    </div> <!-- end row -->
+      <div class="t-chart-item">
+        <h3 class="t-chart-title">@lang('heading.pharmacies')</h3>
+        <canvas id="chart-pharmacies" width="300" height="300"></canvas>
+      </div>
 
-    <div class="row">
-      <div class="col-sm-6">
-        <div class="card widget-flat">
-          <div class="card-body">
-            <div class="float-end">
-              <i class="mdi mdi-currency-usd widget-icon"></i>
-            </div>
-            <h5 class="text-muted fw-normal mt-0" title="Average Revenue">Revenue</h5>
-            <h3 class="mt-3 mb-3">$6,254</h3>
-            <p class="text-muted mb-0">
-              <span class="text-danger me-2"><i class="mdi mdi-arrow-down-bold"></i> 7.00%</span>
-              <span class="text-nowrap">Since last month</span>
-            </p>
-          </div> <!-- end card-body-->
-        </div> <!-- end card-->
-      </div> <!-- end col-->
+      <div class="t-chart-item">
+        <h3 class="t-chart-title">@lang('heading.clients')</h3>
+        <canvas id="chart-clients" width="300" height="300"></canvas>
+      </div>
+    </div>
 
-      <div class="col-sm-6">
-        <div class="card widget-flat">
-          <div class="card-body">
-            <div class="float-end">
-              <i class="mdi mdi-pulse widget-icon"></i>
-            </div>
-            <h5 class="text-muted fw-normal mt-0" title="Growth">Growth</h5>
-            <h3 class="mt-3 mb-3">+ 30.56%</h3>
-            <p class="text-muted mb-0">
-              <span class="text-success me-2"><i class="mdi mdi-arrow-up-bold"></i> 4.87%</span>
-              <span class="text-nowrap">Since last month</span>
-            </p>
-          </div> <!-- end card-body-->
-        </div> <!-- end card-->
-      </div> <!-- end col-->
-    </div> <!-- end row -->
-  </div>
-@stop
+  </main>
+@endsection
+
+
+@section('script')
+  <script>
+    const toLabels = (items) => items.map(item => Object.keys(item)).flat()
+    const toData = (items) => items.map(item => Object.values(item)).flat()
+  </script>
+
+  {{-- chart factory --}}
+  <script>
+    const chartFactor = ({
+      element,
+      labels,
+      label,
+      data
+    }) => {
+      return new Chart(element, {
+        type: 'line',
+        // type: 'polarArea',
+        data: {
+          labels,
+          datasets: [{
+            label,
+            data,
+            backgroundColor: 'rgb(88 ,143 ,244)',
+            borderWidth: 1,
+            borderColor: 'rgb(88 ,143 ,244)',
+          }]
+        },
+        options: {
+          responsive: true,
+          plugins: {
+            // legend: {
+            //   display: false
+            // }
+          }
+        }
+      });
+    }
+  </script>
+
+  <script>
+    // orders
+    axios.get('/api/report/orders').then(res => {
+      console.log(res.data)
+
+      chartFactor({
+        label: '@lang("heading.orders")',
+        element: el('#chart-orders').getContext('2d'),
+        labels: toLabels(res.data),
+        data: toData(res.data)
+      });
+    })
+
+    // pharmacies
+    // axios.get('/api/report/pharmacies').then(res => {
+    axios.get('/api/report/orders').then(res => {
+      console.log(res.data)
+
+      chartFactor({
+        label: '@lang("heading.pharmacies")',
+        element: el('#chart-pharmacies').getContext('2d'),
+        labels: toLabels(res.data),
+        data: toData(res.data)
+      });
+    })
+
+    // clients
+    // axios.get('/api/report/clients').then(res => {
+    axios.get('/api/report/orders').then(res => {
+      console.log(res.data)
+
+      chartFactor({
+        label: '@lang("heading.clients")',
+        element: el('#chart-clients').getContext('2d'),
+        labels: toLabels(res.data),
+        data: toData(res.data)
+      });
+    })
+  </script>
+@endsection
