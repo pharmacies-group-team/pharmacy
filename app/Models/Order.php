fde8ea203b40c7a8307da3a61a16427d0ec26e17
Shatha <65645778+shatha-albajly@users.noreply.github.com>
2022-05-20 19:08:55 +0300

    add relationships with perodic_orders model

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -7,74 +7,82 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Order extends Model
 {
-    use HasFactory, SoftDeletes;
+  use HasFactory, SoftDeletes;
 
-    protected $guarded = [];
+  protected $guarded = [];
 
-    /**
-     * Get Order Invoice
-     */
-    public function Invoice(): HasOne
-    {
-        return $this->hasOne(Invoice::class);
-    }
+  /**
+   * Get Order Invoice
+   */
+  public function Invoice(): HasOne
+  {
+    return $this->hasOne(Invoice::class);
+  }
 
-    /**
-     * Get Order Quotation
-     */
-    public function quotation(): HasOne
-    {
-      return $this->hasOne(Quotation::class);
-    }
+  /**
+   * Get Order Quotation
+   */
+  public function quotation(): HasOne
+  {
+    return $this->hasOne(Quotation::class);
+  }
 
-    /**
-     * Get Pharmacy
-     */
-    public function pharmacy(): BelongsTo
-    {
-        return $this->belongsTo(User::class,'pharmacy_id');
-    }
+  /**
+   * Get Pharmacy
+   */
+  public function pharmacy(): BelongsTo
+  {
+    return $this->belongsTo(User::class, 'pharmacy_id');
+  }
 
-    /**
-     * Get Client
-     */
-    public function user(): BelongsTo
-    {
-        return $this->belongsTo(User::class, 'user_id');
-    }
+  /**
+   * Get Client
+   */
+  public function user(): BelongsTo
+  {
+    return $this->belongsTo(User::class, 'user_id');
+  }
 
-    /**
-     * validation
-     */
-    public static function roles()
-    {
-        return
-          [
-            'image' => 'required_without:order|image|mimes:jpeg,jpg,png,svg|max:2048',
-            'order' => 'required_without:image|string|max:255|nullable'
-          ];
-    }
+  // get orders perodic orders
+  public function PerodicOrders(): HasOne
+  {
+    return $this->hasOne(PerodicOrder::class);
+  }
 
-    /**
-     * messages
-     */
-    public static function messages()
-    {
-        return
-          [
-            'image.required_without' => 'يجب إدخال أحد الحقلين.',
-            'image.image'            => 'يجب أن يكون الحقل المُدخل صورة.',
-            'image.mimes'            => 'يجب أن تكون الصورة ملفًا من النوع jpeg,jpg,png,svg.',
-            'image.max'              => 'يجب ألا تكون الصورة أكبر من 2048 كيلوبايت.',
-            'order.required_without' => 'يجب إدخال أحد الحقلين',
-            'order.string'           => 'يجب أن يكون الحقل سلسلة نصية.',
-            'order.max'              => 'يجب ألا يكون النص أكبر من 255 من الأحرف.'
-          ];
-    }
+  /**
+   * validation
+   */
+  public static function roles()
+  {
+    return
+      [
+        'image' => 'required_without:order|image|mimes:jpeg,jpg,png,svg|max:2048',
+        'order' => 'required_without:image|string|max:255|nullable'
+      ];
+  }
+
+  /**
+   * messages
+   */
+  public static function messages()
+  {
+    return
+      [
+        'image.required_without' => 'يجب إدخال أحد الحقلين.',
+        'image.image'            => 'يجب أن يكون الحقل المُدخل صورة.',
+        'image.mimes'            => 'يجب أن تكون الصورة ملفًا من النوع jpeg,jpg,png,svg.',
+        'image.max'              => 'يجب ألا تكون الصورة أكبر من 2048 كيلوبايت.',
+        'order.required_without' => 'يجب إدخال أحد الحقلين',
+        'order.string'           => 'يجب أن يكون الحقل سلسلة نصية.',
+        'order.max'              => 'يجب ألا يكون النص أكبر من 255 من الأحرف.'
+      ];
+  }
 
   protected static function newFactory()
   {
--- app/Models/PerodicOrder.php
+++ app/Models/PerodicOrder.php
@@ -3,9 +3,32 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class PerodicOrder extends Model
 {
-    use HasFactory;
+  // use HasFactory, SoftDeletes;
+  // protected $guarded = [];
+
+
+
+  /**
+   * Get users
+   */
+
+  public function user(): BelongsTo
+  {
+    return $this->belongsTo(User::class, 'user_id');
+  }
+
+
+  /**
+   * Get orders
+   */
+  public function order(): BelongsTo
+  {
+    return $this->belongsTo(Order::class, 'order_id');
+  }
 }
