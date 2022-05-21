c6faf03176bc463299382c46e05dc7807b8d6908
Ahlam-Mohammed <ahlam.m.alfashq@gmail.com>
2022-05-21 23:55:21 +0300

    Create PerodicOrderSeeder.php

---
+++ database/seeders/PerodicOrderSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\PerodicOrder;
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+
+class PerodicOrderSeeder extends Seeder
+{
+  /**
+   * Run the database seeds.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    //
+    PerodicOrder::create(
+      ['order_id' => 1, 'is_active' => 1, 'start_date' => Carbon::create('2022', '05', '21'), 'perodic_date_type' => 'weekly'],
+      ['order_id' => 2, 'is_active' => 1, 'start_date' => Carbon::create('2022', '05', '22'), 'perodic_date_type' => 'monthly'],
+      ['order_id' => 3, 'is_active' => 0, 'start_date' => Carbon::create('2022', '06', '21'), 'perodic_date_type' => 'weekly']
+    );
+  }
+}
