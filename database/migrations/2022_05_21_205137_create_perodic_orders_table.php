f802746886acef6fb930b5b145a3abf9aaf4e819
Ahlam-Mohammed <ahlam.m.alfashq@gmail.com>
2022-05-21 23:55:15 +0300

    Update 2022_05_21_205137_create_perodic_orders_table.php

--- database/migrations/2022_05_21_205137_create_perodic_orders_table.php
+++ database/migrations/2022_05_21_205137_create_perodic_orders_table.php
@@ -16,7 +16,10 @@ return new class extends Migration
       Schema::create('perodic_orders', function (Blueprint $table) {
         $table->id();
 
-        $table->date('next_order');
+        $table->date('next_order_date');
+        $table->date('start_date');
+        $table->string('perodic_date_type');
+        $table->boolean('is_active');
 
         $table->foreignId('order_id');
         $table->foreign('order_id')->references('id')->on('orders')->onDelete('cascade');
