0a2990bb3cfae1486975219c710229bed74dfb3d
Ahlam-Mohammed <ahlam.m.alfashq@gmail.com>
2022-05-22 00:03:01 +0300

    Update PerodicOrder.php

--- app/Models/PerodicOrder.php
+++ app/Models/PerodicOrder.php
@@ -4,8 +4,28 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class PerodicOrder extends Model
 {
-    use HasFactory;
+   use HasFactory, SoftDeletes;
+   protected $guarded = [];
+
+    /**
+     * Get users
+     */
+
+    public function user(): BelongsTo
+    {
+      return $this->belongsTo(User::class, 'user_id');
+    }
+
+    /**
+     * Get orders
+     */
+    public function order(): BelongsTo
+    {
+      return $this->belongsTo(Order::class, 'order_id');
+    }
 }
