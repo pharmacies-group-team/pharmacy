9a69b1ec95de0140f57035d8bf711de0bc4ca0ad
naif <nayef7138@gmail.com>
2022-05-22 02:07:22 +0300

    update: admin\cities UI

--- resources/views/admin/cities.blade.php
+++ resources/views/admin/cities.blade.php
@@ -4,59 +4,60 @@
   <x-alert type="status" />
 
   <main class="ads" x-data="{ id: null, city: {{ json_encode(old()) }} ?? {}, addModal: false, editModal: false, deleteModal: false }">
-    <div class="container">
-      <section class="section-header">
+    <div>
+      <section class="section-header t-card">
         <h2 class="text-large">إدارة طرق الدفع</h2>
-        <button class="btn" @click="addModal = true; city = {{ json_encode(old()) }} ?? {}">اضافة مدينة</button>
+        <button class="btn" @click="addModal = true; city = {{ json_encode(old()) }} ?? {}">اضافة
+          مدينة</button>
       </section>
 
       <div class="table-wrapper">
         {{-- table --}}
         <table class="table">
           <thead>
-          <tr>
-            <th>
-              رقم المدينة
-            </th>
-            <th>
-              اسم المدينة
-            </th>
-            <th>
-              تاريخ الإنشاء
-            </th>
-            <th></th>
-          </tr>
+            <tr>
+              <th>
+                رقم المدينة
+              </th>
+              <th>
+                اسم المدينة
+              </th>
+              <th>
+                تاريخ الإنشاء
+              </th>
+              <th></th>
+            </tr>
           </thead>
           <tbody>
-          @foreach ($cities as $city)
-            <tr>
-              <td>
-                {{ $city->id }}
+            @foreach ($cities as $city)
+              <tr>
+                <td>
+                  {{ $city->id }}
 
-              </td>
+                </td>
 
-              <td>{{ $city->name }}</td>
+                <td>{{ $city->name }}</td>
 
-              <td>{{ $city->created_at->format('Y-m-d') }}</td>
+                <td>{{ $city->created_at->format('Y-m-d') }}</td>
 
-              <td class="table-action" x-data="{ editModal: false, deleteModal: false }">
-                {{-- edit --}}
-                <button @click="editModal = true">
-                  <x-icon icon='edit' />
-                </button>
-                <x-admin.edit-city :city='$city' />
-
-
-                {{-- remove ads --}}
-                <button @click="deleteModal = true">
-                  <x-icon icon='remove' />
-                </button>
-                <x-admin.delete-city :city="$city" />
-              </td>
+                <td class="table-action" x-data="{ editModal: false, deleteModal: false }">
+                  {{-- edit --}}
+                  <button @click="editModal = true">
+                    <x-icon icon='edit' />
+                  </button>
+                  <x-admin.edit-city :city='$city' />
 
 
-            </tr>
-          @endforeach
+                  {{-- remove ads --}}
+                  <button @click="deleteModal = true">
+                    <x-icon icon='remove' />
+                  </button>
+                  <x-admin.delete-city :city="$city" />
+                </td>
+
+
+              </tr>
+            @endforeach
           </tbody>
         </table>
       </div>
@@ -74,9 +75,9 @@
           <div class="form-group">
             <label for="ad-name-label">الاسم</label>
             <input id="ad-name-label" name="name" :value="city.name" type="text"
-                   class="form-control @error('name') is-invalid @enderror" placeholder="أسم المدينة" />
+              class="form-control @error('name') is-invalid @enderror" placeholder="أسم المدينة" />
             @error('name')
-            <span class="invalid-feedback">{{ $message }}</span>
+              <span class="invalid-feedback">{{ $message }}</span>
             @enderror
           </div>
 
