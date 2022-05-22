fc4b7c97d42d6088c821cac45d81ba7a81a4986e
naif <nayef7138@gmail.com>
2022-05-23 00:46:37 +0300

    contact us

--- lang/ar/form.php
+++ lang/ar/form.php
@@ -3,17 +3,25 @@
 
 return [
   "name" => "اسم",
+  "username" => "اسم المستخدم",
+
   "email" => "الايميل",
+
   "password" => "كلمه السر",
   "confirm-password" => "تاكيد كلمه السر الجديدة",
   "old-password" => "كلمه السر القديمة",
   "new-password" => "تاكيد كلمه السر الجديدة",
   "confirm-new-password" => "تاكيد كلمه السر الجديدة",
+
   "save" => "حفظ",
   "update" => "تحديث",
   "edit" => "تعديل",
+
   "avatar" => "صوره البروفايل",
   "add-image" => "أضف صورة",
   "update-image" => "تحديث الصورة",
-  "phone" => "رقم الهاتف"
+
+  "phone" => "رقم الهاتف",
+
+  "message" => 'الرسالة',
 ];
