e748cad5820542d68f20c50e1acc22ef9597865a
naif <nayef7138@gmail.com>
2022-05-22 11:36:48 +0300

    update: add random chart data

--- resources/views/pharmacy/index.blade.php
+++ resources/views/pharmacy/index.blade.php
@@ -28,125 +28,48 @@
 
 
 @section('script')
-  <script>
-    const toLabels = (items) => items.map(item => Object.keys(item)).flat()
-    const toData = (items) => items.map(item => Object.values(item)).flat()
-  </script>
-
-
-  {{-- chart settings --}}
-  <script>
-    const CHART_LABELS = [
-      'January',
-      'February',
-      'March',
-      'April',
-      'May',
-      'June',
-      'July',
-      'August',
-      'September',
-      'October',
-      'November',
-      'December'
-    ];
-
-    const CHART_COLORS = [
-      'rgb(88 ,143 ,244)',
-      '#f44336 ',
-      ' #e91e63',
-      '#9c27b0 ',
-      ' #673ab7',
-      '#3f51b5 ',
-      '#2196f3 ',
-      '#4caf50 ',
-      '#cddc39',
-      '#ffc107',
-      '#ff9800',
-      '#ff5722',
-      '#607d8b',
-    ];
-  </script>
-
-  {{-- chart factory --}}
-  <script>
-    const chartFactor = ({
-      element,
-      type = 'line',
-      labels,
-      label,
-      data
-    }) => {
-      return new Chart(element, {
-        type,
-        // type: 'polarArea',
-        data: {
-          labels,
-          datasets: [{
-            label,
-            data,
-            // backgroundColor: 'rgb(88 ,143 ,244)',
-            backgroundColor: CHART_COLORS,
-            borderWidth: 1,
-            borderColor: CHART_COLORS,
-          }]
-        },
-        options: {
-          animation: false,
-          responsive: true,
-          plugins: {
-            // legend: {
-            //   display: false
-            // }
-          },
-          scales: {
-            y: {
-              // type: 'linear',
-              min: 0,
-            }
-          }
-        }
-      });
-    }
-  </script>
+  <x-chart />
 
   <script>
     // orders
     axios.get('/api/report/orders').then(res => {
-      console.log(res.data)
 
       chartFactor({
         label: '@lang('heading.orders')',
         element: el('#chart-orders').getContext('2d'),
-        labels: toLabels(res.data),
-        data: toData(res.data)
+        // labels: toLabels(res.data),
+        // data: toData(res.data)
+        labels: toLabels(fakeChartData()),
+        data: toData(fakeChartData()),
       });
     })
 
     // clients
     // axios.get('/api/report/clients').then(res => {
     axios.get('/api/report/orders').then(res => {
-      console.log(res.data)
 
       chartFactor({
         label: '@lang('heading.clients')',
         element: el('#chart-clients').getContext('2d'),
-        labels: toLabels(res.data),
-        data: toData(res.data)
+        // labels: toLabels(res.data),
+        // data: toData(res.data)
+        labels: toLabels(fakeChartData()),
+        data: toData(fakeChartData()),
       });
     })
 
     // profits
     // axios.get('/api/report/profits').then(res => {
     axios.get('/api/report/orders').then(res => {
-      console.log(res.data)
 
       chartFactor({
-        label: '@lang('heading.clients')',
+        label: '@lang('heading.profits')',
         // type: 'radar',
         element: el('#chart-profits').getContext('2d'),
-        labels: toLabels(res.data),
-        data: toData(res.data)
+        // labels: toLabels(res.data),
+        // data: toData(res.data)
+        labels: toLabels(fakeChartData()),
+        data: toData(fakeChartData()),
       });
     })
   </script>
